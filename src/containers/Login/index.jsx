adc0791eb689dd834c4660889a4ce7956b2917ee
Gabriel Felix <gabriel.alves2208@gmail.com>
2024-09-14 15:50:39 -0300

    primeiro commit da tela de login

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,8 +1,41 @@
+import Logo from '../../assets/logo-login.svg';
+import { Button } from '../../components/Button';
+import {
+    Container,
+    Form,
+    InputContainer,
+    LeftContainer,
+    RightContainer,
+    Title
+} from './styles';
 export function Login() {
-
     return (
-        <div>
-            Login Page
-        </div>
-    )
+        <Container>
+            <LeftContainer>
+                <img src={Logo} alt='logo-devburger' />
+            </LeftContainer>
+            <RightContainer>
+                <Title>
+                    Olá, seja bem vindo ao <span>Dev Burguer!</span>
+                    <br />
+                    Acesse com seu <span>Login e senha.</span>
+                </Title>
+                <Form>
+                    <InputContainer>
+                        <label>Email</label>
+                        <input type="email" />
+                    </InputContainer>
+
+                    <InputContainer>
+                        <label>Senha</label>
+                        <input type="password" />
+                    </InputContainer>
+                    <Button>Entrar</Button>
+                </Form>
+                <p>
+                    Não possui uma conta? <a>Cadastre-se.</a>
+                </p>
+            </RightContainer>
+        </Container>
+    );
 }
