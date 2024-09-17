105c8bcd3e71903a82a1021b75b54fb16ebcfc60
Gabriel Felix <gabriel.alves2208@gmail.com>
2024-09-17 17:46:37 -0300

    commit criando tela de login

---
+++ src/containers/Register/index.jsx
@@ -0,0 +1,107 @@
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { api } from '../../services/api';
+import { toast } from 'react-toastify';
+
+import Logo from '../../assets/logo-login.svg';
+import { Button } from '../../components/Button';
+import {
+    Container,
+    Form,
+    InputContainer,
+    LeftContainer,
+    RightContainer,
+    Title
+} from './styles';
+export function Register() {
+    const schema = yup
+        .object({
+            name: yup
+                .string()
+                .required('O nome é obrigatório'),
+            email: yup
+                .string()
+                .email('Digite um e-mail válido')
+                .required('O e-mail é obrigatório'),
+            password: yup
+                .string()
+                .min(6, 'Sua senha deve ter no mínimo 6 caracteres.')
+                .required('Digite uma senha'),
+            confirmPassword: yup
+                .string()
+                .oneOf([yup.ref('password')], 'As senhas devem ser iguais')
+                .required('Confirme sua senha'),
+        })
+        .required();
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(schema),
+    });
+
+    console.log(errors);
+
+    const onSubmit = async (data) => {
+        const response = await toast.promise(
+            api.post('/users', {
+                name: data.name,
+                email: data.email,
+                password: data.password,
+            }),
+            {
+                pending: 'Verificando seus dados',
+                success: 'Cadastro efetuado com Sucesso! 👌',
+                error: 'Ops, algo deu errado! Tente novamente. 🤯'
+            },
+        );
+
+        console.log(response);
+    };
+
+    return (
+        <Container>
+            <LeftContainer>
+                <img src={Logo} alt='logo-devburger' />
+            </LeftContainer>
+            <RightContainer>
+                <Title>
+                    Criar Conta
+                </Title>
+                <Form onSubmit={handleSubmit(onSubmit)}>
+                    <InputContainer>
+                        <label>Nome</label>
+                        <input type="text" {...register('name')} />
+                        <p>{errors?.name?.message}</p>
+                    </InputContainer>
+
+                    <InputContainer>
+                        <label>Email</label>
+                        <input type="email" {...register('email')} />
+                        <p>{errors?.email?.message}</p>
+                    </InputContainer>
+
+                    <InputContainer>
+                        <label>Senha</label>
+                        <input type="password" {...register('password')} />
+                        <p>{errors?.password?.message}</p>
+                    </InputContainer>
+
+                    <InputContainer>
+                        <label>Confirmar Senha</label>
+                        <input type="password" {...register('confirmPassword')} />
+                        <p>{errors?.confirmPassword?.message}</p>
+                    </InputContainer>
+
+                    <Button type="submit">Criar Conta</Button>
+                </Form>
+                <p>
+                    Já possui uma conta ? <a>Clique aqui.</a>
+                </p>
+            </RightContainer>
+        </Container>
+    );
+}
---
+++ src/containers/Register/styles.js
@@ -0,0 +1,98 @@
+import styled from 'styled-components';
+
+import BackgroundLogin from '../../assets/background-login.svg';
+import Background from '../../assets/background.svg';
+
+export const Container = styled.div`
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+`;
+
+export const LeftContainer = styled.div`
+    background: url('${BackgroundLogin}');
+    background-size: cover;
+    background-position: center;
+
+    height: 100%;
+    width: 100%;
+    max-width: 50%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    img {
+        width: 80%;
+    }
+`;
+
+export const RightContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    height: 100%;
+    width: 100%;
+    max-width: 50%;
+
+    background: url('${Background}');
+    background-color: #1E1E1E;
+
+    p {
+        font-size: 18px;
+        font-weight: 800;
+        color: #fff;
+
+    a {
+        text-decoration: underline;
+        color: #9758A6;
+    }
+    }
+`;
+
+export const Title = styled.h2`
+    font-family: "Road Rage", sans-serif;
+    font-size: 40px;
+    font-weight: 500;
+    color: #9758A6;
+    text-align: center;
+`;
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    padding: 20px;
+    width: 100%;
+    max-width: 400px;
+`;
+
+export const InputContainer = styled.div `
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    width: 100%;
+
+    input {
+        width: 100%;
+        border: none;
+        height: 52px;
+        border-radius: 5px;
+        padding: 0 16px;
+    }
+
+    label {
+        font-size: 18px;
+        font-weight: 600;
+        color: #fff;
+    }
+
+    p {
+        color: #FF5C61;
+        font-size: 14px;
+        text-align: center;
+        height: 10px;
+    }
+`;
