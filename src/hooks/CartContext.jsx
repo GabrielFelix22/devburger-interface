2eb2bd7216da611f2a6cdbf9f884046eb34bfcfb
Gabriel Felix <gabriel.alves2208@gmail.com>
2024-10-10 19:42:14 -0300

    commit do clearCart no CartContext e organização das importações

--- src/hooks/CartContext.jsx
+++ src/hooks/CartContext.jsx
@@ -27,7 +27,9 @@ export const CartProvider = ({ children }) => {
     };
 
     const clearCart = () => {
-
+        setCartProducts([]);
+        
+        updateLocalStorage([]);
     };
 
     const deleteProduct = (productId) => {
