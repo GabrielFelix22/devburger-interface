794af6f705b21d6ebb3453dcf0aa22e39e5127b2
Gabriel Felix <gabriel.alves2208@gmail.com>
2024-10-12 17:10:31 -0300

    commit finalizando parte dos pedidos com o botão finalizar pedido funcionando e já confirmando o pedido do carrinho

--- src/components/CartResume/index.jsx
+++ src/components/CartResume/index.jsx
@@ -1,7 +1,59 @@
+import { toast } from 'react-toastify';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useCart } from '../../hooks/CartContext';
+import { api } from '../../services/api';
+import { formatPrice } from '../../utils/formatPrice';
+
 import { Button } from '../Button';
 import { Container } from './styles';
 
 export function CartResume() {
+    const [finalPrice, setFinalPrice] = useState(0);
+    const [deliveryTax] = useState(500);
+
+    const navigate = useNavigate();
+
+    const { cartProducts, clearCart } = useCart();
+
+    useEffect(() => {
+        const sumAllItems = cartProducts.reduce((acc, product) => {
+            return product.price * product.quantity + acc;
+        }, 0);
+
+        setFinalPrice(sumAllItems);
+    }, [cartProducts]);
+
+    const submitOrder = async () => {
+        const products = cartProducts.map((product) => {
+            return { id: product.id, quantity: product.quantity };
+        });
+
+        try {
+            const { status } = await api.post(
+                '/orders',
+                { products },
+                {
+                    validateStatus: () => true,
+                });
+
+            if (status === 200 || status === 201) {
+                setTimeout(() => {
+                    navigate('/');
+                }, 2000);
+                
+                clearCart();
+                toast.success('Pedido realizado com sucesso!');
+            } else if (status === 400) {
+                toast.error('Falha ao realizar o seu pedido');
+            } else {
+                throw new Error();
+            }
+
+        } catch (error) {
+            toast.error('😭 Fala no Sistema! Tente novamente');
+        }
+    };
 
     return (
         <div>
@@ -9,16 +61,16 @@ export function CartResume() {
                 <div className="container-top">
                     <h2 className='title'>Resumo do Pedido</h2>
                     <p className='items'>Itens</p>
-                    <p className='items-price'>R$ 20,00</p>
+                    <p className='items-price'>{formatPrice(finalPrice)}</p>
                     <p className='delivery-tax'>Taxa de Entrega</p>
-                    <p className='delivery-tax-price'>R$ 5,00</p>
+                    <p className='delivery-tax-price'>{formatPrice(deliveryTax)}</p>
                 </div>
                 <div className="container-bottom">
                     <p>Total</p>
-                    <p>R$ 25,00</p>
+                    <p>{formatPrice(finalPrice + deliveryTax)}</p>
                 </div>
             </Container>
-            <Button>Finalizar Compra</Button>
+            <Button onClick={submitOrder}>Finalizar Compra</Button>
         </div>
     );
 }
