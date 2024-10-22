3e37ae945653df1093540af23db8d3cb5e9a44d9
Gabriel Felix <gabriel.alves2208@gmail.com>
2024-10-21 22:34:34 -0300

    commit configurando o stripe no front-end

---
+++ src/config/stripeConfig.js
@@ -0,0 +1,5 @@
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('pk_test_51Q9Bh5E4TkWcw6G05uZVrz3af4CY2aBSxWRortL08hHtEy7nibaPQdSeboFOawOj0UgoUFQZsImxrdm99bP5stpV00YjbJBK8U');
+
+export default stripePromise;
