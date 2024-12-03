4ae4620657aba63589f027568fff70502a17c5bb
Gabriel Felix <gabriel.alves2208@gmail.com>
2024-12-03 15:23:01 -0300

    commit incluindo o theme para uso das cores de forma mais prática.

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 export const Container = styled.div `
     background-color: #1f1f1f;
