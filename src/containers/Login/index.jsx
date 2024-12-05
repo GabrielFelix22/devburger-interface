888b0f38729cf5be8b84aa34ee146ec3796f9e80
Gabriel Felix <gabriel.alves2208@gmail.com>
2024-12-05 17:27:17 -0300

    commit criando e configurando a tela de admin, configurando a navegação de usuário comum, para usuário admin.

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,99 +1,103 @@
-import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-import { api } from '../../services/api';
-import { toast } from 'react-toastify';
+import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import * as yup from 'yup';
 import { useUser } from '../../hooks/UserContext';
+import { api } from '../../services/api';
 
 import Logo from '../../assets/logo-login.svg';
 import { Button } from '../../components/Button';
 import {
-    Container,
-    Form,
-    InputContainer,
-    LeftContainer,
-    RightContainer,
-    Title,
-    Link,
+  Container,
+  Form,
+  InputContainer,
+  LeftContainer,
+  Link,
+  RightContainer,
+  Title,
 } from './styles';
 export function Login() {
-    const navigate = useNavigate();
-    const { putUserData } = useUser();
+  const navigate = useNavigate();
+  const { putUserData } = useUser();
 
-    const schema = yup
-        .object({
-            email: yup
-                .string()
-                .email('Digite um e-mail válido')
-                .required('O e-mail é obrigatório'),
-            password: yup
-                .string()
-                .min(6, 'Sua senha deve ter no mínimo 6 caracteres.')
-                .required('Digite uma senha'),
-        })
-        .required();
+  const schema = yup
+    .object({
+      email: yup
+        .string()
+        .email('Digite um e-mail válido')
+        .required('O e-mail é obrigatório'),
+      password: yup
+        .string()
+        .min(6, 'Sua senha deve ter no mínimo 6 caracteres.')
+        .required('Digite uma senha'),
+    })
+    .required();
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm({
-        resolver: yupResolver(schema),
-    });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-    const onSubmit = async (data) => {
-        const { data: userData } = await toast.promise(
-            api.post('/session', {
-                email: data.email,
-                password: data.password,
-            }),
-            {
-                pending: 'Verificando seus dados',
-                success: {
-                    render() {
-                        setTimeout(() => {
-                            navigate('/');
-                        }, 2000);
-                        return 'Seja bem-vindo(a) 👌';
-                    },
-                },
-                error: 'Email ou Senha Incorretos 🤯',
-            },
-        );
-
-        putUserData(userData);
-    };
-
-    return (
-        <Container>
-            <LeftContainer>
-                <img src={Logo} alt='logo-devburger' />
-            </LeftContainer>
-            <RightContainer>
-                <Title>
-                    Olá, seja bem vindo ao <span>Dev Burguer!</span>
-                    <br />
-                    Acesse com seu <span>Login e senha.</span>
-                </Title>
-                <Form onSubmit={handleSubmit(onSubmit)}>
-                    <InputContainer>
-                        <label>Email</label>
-                        <input type="email" {...register('email')} />
-                        <p>{errors?.email?.message}</p>
-                    </InputContainer>
-
-                    <InputContainer>
-                        <label>Senha</label>
-                        <input type="password" {...register('password')} />
-                        <p>{errors?.password?.message}</p>
-                    </InputContainer>
-                    <Button type="submit">Entrar</Button>
-                </Form>
-                <p>
-                    Não possui uma conta? <Link to="/cadastro">Cadastre-se.</Link>
-                </p>
-            </RightContainer>
-        </Container>
+  const onSubmit = async (data) => {
+    const { data: userData } = await toast.promise(
+      api.post('/session', {
+        email: data.email,
+        password: data.password,
+      }),
+      {
+        pending: 'Verificando seus dados',
+        success: {
+          render() {
+            setTimeout(() => {
+              if (userData?.admin) {
+                navigate('/admin/home');
+              } else {
+                navigate('/');
+              }
+            }, 2000);
+            return 'Seja bem-vindo(a) 👌';
+          },
+        },
+        error: 'Email ou Senha Incorretos 🤯',
+      },
     );
+
+    putUserData(userData);
+  };
+
+  return (
+    <Container>
+      <LeftContainer>
+        <img src={Logo} alt="logo-devburger" />
+      </LeftContainer>
+      <RightContainer>
+        <Title>
+          Olá, seja bem vindo ao <span>Dev Burguer!</span>
+          <br />
+          Acesse com seu <span>Login e senha.</span>
+        </Title>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <InputContainer>
+            <label>Email</label>
+            <input type="email" {...register('email')} />
+            <p>{errors?.email?.message}</p>
+          </InputContainer>
+
+          <InputContainer>
+            <label>Senha</label>
+            <input type="password" {...register('password')} />
+            <p>{errors?.password?.message}</p>
+          </InputContainer>
+          <Button type="submit">Entrar</Button>
+        </Form>
+        <p>
+          Não possui uma conta? <Link to="/cadastro">Cadastre-se.</Link>
+        </p>
+      </RightContainer>
+    </Container>
+  );
 }
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,7 +1,9 @@
+export * from './Admin';
 export * from './Cart';
+export * from './Checkout';
+export * from './CompletePayment';
 export * from './Home';
 export * from './Login';
 export * from './Menu';
 export * from './Register';
-export * from './Checkout';
-export * from './CompletePayment';
+
