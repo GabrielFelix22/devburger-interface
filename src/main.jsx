65779d9a4f28ec63b2cd2dc7dbd4cb423fc41c90
Gabriel Felix <gabriel.alves2208@gmail.com>
2024-12-05 16:35:24 -0300

    commit ajustando as routes e deixando mais profissional com o Outlet. Criando a pasta de layouts para fazer essa mudança nas routes.

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,24 @@
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 import { Elements } from '@stripe/react-stripe-js';
 import { ThemeProvider } from 'styled-components';
 import stripePromise from './config/stripeConfig';
 import AppProvider from './hooks';
-import { router } from './routes';
+import { Router } from './routes';
 import GlobalStyles from './styles/globalStyles';
 import { standardTheme } from './styles/themes/standard';
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={{ standardTheme }}>
       <AppProvider>
         <Elements stripe={stripePromise}>
-          <RouterProvider router={router} />
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
         </Elements>
         <GlobalStyles />
         <ToastContainer autoClose={2000} theme="colored" />
