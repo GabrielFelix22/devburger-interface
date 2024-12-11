327f60d117cac0d4545df4ba806ae86bd0d301e5
Gabriel Felix <gabriel.alves2208@gmail.com>
2024-12-11 12:09:24 -0300

    commit adicionando background fixo em qual rota estiver acessado no painel de admin.

--- src/components/SideNavAdmin/index.jsx
+++ src/components/SideNavAdmin/index.jsx
@@ -1,4 +1,5 @@
 import { SignOut } from '@phosphor-icons/react';
+import { useResolvedPath } from 'react-router-dom';
 import Logo from '../../assets/logo-login.svg';
 import { useUser } from '../../hooks/UserContext';
 import { navLinks } from './navLinks';
@@ -6,13 +7,18 @@ import { Container, Footer, NavLink, NavLinkContainer } from './styles';
 
 export function SideNavAdmin() {
   const { logout } = useUser();
+  const { pathname } = useResolvedPath();
 
   return (
     <Container>
       <img src={Logo} alt="logo devburger" />
       <NavLinkContainer>
         {navLinks.map((link) => (
-          <NavLink key={link.id} to={link.path}>
+          <NavLink
+            key={link.id}
+            to={link.path}
+            $isActive={pathname === link.path}
+          >
             {link.icon}
             <span>{link.label}</span>
           </NavLink>
