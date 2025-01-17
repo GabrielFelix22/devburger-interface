24d9b6aefcdf670b24791390dbc74faf94ed0db6
Gabriel Felix <gabriel.alves2208@gmail.com>
2025-01-17 00:08:03 -0300

    commit: Terminando de configurar a tela de produtos

--- src/containers/Admin/NewProduct/index.jsx
+++ src/containers/Admin/NewProduct/index.jsx
@@ -1,10 +1,13 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Image } from '@phosphor-icons/react';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 
+import { useEffect, useState } from 'react';
+import { api } from '../../../services/api';
 import {
   Container,
+  ErrorMessage,
   Form,
   Input,
   InputGroup,
@@ -14,46 +17,87 @@ import {
   SubmitButton,
 } from './styles';
 
-const schema = yup
-  .object({
-    firstName: yup.string().required(),
-    age: yup.number().positive().integer().required(),
-  })
-  .required();
+const schema = yup.object({
+  name: yup.string().required(),
+  price: yup.number().positive().required(),
+  category: yup.object().required(),
+  file: yup.mixed(),
+});
 
 export function NewProduct() {
+  const [fileName, setFileName] = useState(null);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    async function loadCategories() {
+      const { data } = await api.get('/categories');
+
+      setCategories(data);
+    }
+
+    loadCategories();
+  }, []);
+
   const {
     register,
     handleSubmit,
+    control,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
   });
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
     <Container>
-      <Form>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <InputGroup>
           <Label>Nome</Label>
-          <Input />
+          <Input type="text" {...register('name')} />
+          <ErrorMessage>{errors?.name?.message}</ErrorMessage>
         </InputGroup>
 
         <InputGroup>
           <Label>Preço</Label>
-          <Input />
+          <Input type="number" {...register('price')} />
+          <ErrorMessage>{errors?.price?.message}</ErrorMessage>
         </InputGroup>
 
         <InputGroup>
           <LabelUpload>
             <Image />
-            <input type="file" />
+            <input
+              type="file"
+              {...register('file')}
+              accept="image/png, image/jpeg"
+              onChange={(value) => {
+                setFileName(value?.target?.files[0]?.name);
+                register('file').onChange(value);
+              }}
+            />
+
+            {fileName || 'Upload do Produto'}
           </LabelUpload>
         </InputGroup>
 
         <InputGroup>
           <Label>Categoria</Label>
-          <Select />
+          <Controller
+            name="category"
+            control={control}
+            render={(field) => (
+              <Select
+                {...field}
+                options={categories}
+                getOptionLabel={(category) => category.name}
+                getOptionValue={(category) => category.id}
+                placeholder="Categorias"
+                menuPortalTarget={document.body}
+              />
+            )}
+          />
         </InputGroup>
 
         <SubmitButton>Adicionar Produto</SubmitButton>
